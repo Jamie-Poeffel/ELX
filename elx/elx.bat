cb07a2d4b2472c8b70f915c9f9c1c4c33764b469
Jamie Poeffel <Jamie_Poeffel@sluz.ch>
2024-10-21 09:55:03 +0200

    delete file added and implemtente

--- elx/elx.bat
+++ elx/elx.bat
@@ -1,12 +1,14 @@
 @echo off
 
-
 if "%1" == "install" (
     PowerShell -NoProfile -ExecutionPolicy Bypass -File "C:\Windows\System32\ELW\install.ps1"
 ) else if "%1" == "--help" (
     PowerShell -NoProfile -ExecutionPolicy Bypass -File "C:\Windows\System32\ELW\help.ps1"
 ) else if "%1" == "setup" (
     PowerShell -NoProfile -ExecutionPolicy Bypass -File "C:\Windows\System32\ELW\setup.ps1"
-)else (
+) else if "%1" == "--Delete" (
+    PowerShell -NoProfile -ExecutionPolicy Bypass -File "C:\Windows\System32\ELW\delete.ps1"
+)
+else (
     PowerShell -NoProfile -ExecutionPolicy Bypass -File "C:\Windows\System32\ELW\error.ps1"
 )
