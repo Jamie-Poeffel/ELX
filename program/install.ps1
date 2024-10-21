da7cb70963d7475b55f4959985173ce79d28d931
Jamie Poeffel <Jamie_Poeffel@sluz.ch>
2024-10-21 18:04:11 +0200

    install added

--- program/install.ps1
+++ program/install.ps1
@@ -1,1 +1,25 @@
-Write-Output "hello install"
+$frameworks = @("gito", "c#", "python", "java")
+
+cls
+function Show-Menu {
+    param (
+        [string[]]$options
+    )
+    
+    Write-Host "Select a framework:"
+    for ($i = 0; $i -lt $options.Length; $i++) {
+        Write-Host "$($i + 1). $($options[$i])"
+    }
+    
+    $selection = Read-Host "Enter the number of your choice"
+    
+    if ($selection -match '^\d+$' -and [int]$selection -le $options.Length -and [int]$selection -ge 1) {
+        $chosenFramework = $options[$selection - 1]
+        Write-Host "You selected: $chosenFramework"
+    } else {
+        Write-Host "Invalid selection. Please try again."
+        Show-Menu -options $options  # Wiederhole das Menü bei ungültiger Eingabe
+    }
+}
+
+Show-Menu -options $frameworks
